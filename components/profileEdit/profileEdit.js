e34b4a219065ea55822764221b36f0869920962b
Shawn Farnum <shawnmfarnum@gmail.com>
2016-06-23 13:50:45 -0600

    merging

--- components/profileEdit/profileEdit.js
+++ components/profileEdit/profileEdit.js
@@ -1,11 +1,66 @@
 import React from 'react';
 
-function ProfileEdit() {
-  return (
-   <div className="profileEdit">
-     <p>Details go here!</p>
-   </div>
- );
-}
+var ProfileEdit = React.createClass({
+  render: function(){
+    return (
+      <section>
+         <div class="container">
+             <div class="collapse-card">
+                 <div class="collapse-card__heading">
+                     <h4 class="collapse-card__title">
+                         <i class="fa fa-question-circle fa-2x fa-fw"></i>
+                         Well, hello there
+                     </h4>
+                 </div>
+                 <div class="collapse-card__body">
+                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                     labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                     ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                     dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                     magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                     clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                 </div>
+             </div>
+             <div class="collapse-card">
+                 <div class="collapse-card__heading">
+                     <h4 class="collapse-card__title">
+                         <i class="fa fa-question-circle fa-2x fa-fw"></i>
+                         Gigantomaniac Monster Text, very long, much width, wow
+                     </h4>
+                 </div>
+                 <div class="collapse-card__body">
+                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                     labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                     ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                     dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                     magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                     clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                     <div class="collapse-card__close_handler mt1 align-right mouse-pointer">
+                         Show less <i class="fa fa-chevron-up"></i>
+                     </div>
+                 </div>
+             </div>
+             <div class="collapse-card">
+                 <div class="collapse-card__heading">
+                     <h4 class="collapse-card__title">
+                         <i class="fa fa-question-circle fa-2x fa-fw"></i>
+                         Generic Title
+                     </h4>
+                 </div>
+                 <div class="collapse-card__body">
+                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                     labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                     ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                     dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                     magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                     clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                 </div>
+             </div>
+         </div>
+         </section>
+    )
+  }
+});
+
 
 export default ProfileEdit;
