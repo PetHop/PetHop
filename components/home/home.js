0fd360a977c57e7a93dfeadf210cf8ff40c07176
Milton Menasco <milton@miltonmenasco.com>
2016-06-23 12:21:45 -0600

    Profile Structure and Home Structure complete

--- components/home/home.js
+++ components/home/home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Nav from './../nav/nav.js';
+import Header from './homeHeader.js';
+import InfoUpper from './homeInfoUpper.js';
+import InfoLower from './homeInfoLower.js';
+import Footer from './../footer/footer.js'
 
 function Home() {
   return (
    <div className="home">
-     <p>Details go here!</p>
+   <Header />
+   <Nav />
+   <InfoUpper />
+   <InfoLower />
+   <Footer />
    </div>
  );
 }
---
+++ components/home/homeInfoUpper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+var InfoUpper = React.createClass({
+  render: function() {
+    return (
+     <div className="HomeInfoUpper">
+       <p>this.props.HomeInfoUpper info goes here</p>
+     </div>
+   )
+  }
+});
+export default InfoUpper;
