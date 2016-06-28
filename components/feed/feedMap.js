2013b651ac1655d4122c58b470bc22c30be55e43
Milton Menasco <milton@miltonmenasco.com>
2016-06-28 11:20:42 -0600

    awesome begining of map working

--- components/feed/feedMap.js
+++ components/feed/feedMap.js
@@ -1,65 +1,21 @@
-var React = require('react');
-var ReactGoogleMaps = require('react-googlemaps');
 
-var GoogleMapsAPI = window.google.maps;
-var Map = ReactGoogleMaps.Map;
-var Marker = ReactGoogleMaps.Marker;
-var OverlayView = ReactGoogleMaps.OverlayView;
+ import React from 'react';
+ import { GoogleMap, Marker } from 'react-google-maps';
+ import MapLoader from './maploader.js';
 
-var FeedMap = React.createClass({
+ function FeedMap (props){
+     return (
+       <div id="google-map">
+         <MapLoader>
+         <GoogleMap
+          defaultZoom={14}
+          center={{lat: 46.8787, lng: -114.000}}
+          >
+         <Marker position={{lat: 0, lng: 0}} onClick={() => alert('hello world')} />
+       </GoogleMap>
+     </MapLoader>
+     </div>
+         )
+ };
 
-  render: function() {
-    return (
-          <Map
-            initialZoom={10}
-            initialCenter={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
-
-            <Marker
-              onClick={handleClick}
-              position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)} />
-
-            <OverlayView
-              style={{backgroundColor: '#fff'}}
-              position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
-              <p>Some content</p>
-            </OverlayView>
-          </Map>
-
-          <div>
-
-          </div>
-   )
-  }
-});
-
-export default FeedMap;
-
-// var React = require('react');
-// var ReactGoogleMaps = require('react-googlemaps');
-// var GoogleMapsAPI = window.google.maps;
-//
-// var Map = ReactGoogleMaps.Map;
-// var Marker = ReactGoogleMaps.Marker;
-// var OverlayView = ReactGoogleMaps.OverlayView;
-
-// function handleClick(e) {
-//   console.log('Clicked at position', e.latLng);
-// }
-//
-// React.render(
-//   <Map
-//     initialZoom={10}
-//     initialCenter={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
-//
-//     <Marker
-//       onClick={handleClick}
-//       position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)} />
-//
-//     <OverlayView
-//       style={{backgroundColor: '#fff'}}
-//       position={new GoogleMapsAPI.LatLng(-41.2864, 174.7762)}>
-//       <p>Some content</p>
-//     </OverlayView>
-//   </Map>,
-//   mountNode
-// );
+ module.exports = FeedMap;
