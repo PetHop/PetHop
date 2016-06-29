f8be9413769f385992e7c935f0e109abaeecb227
moosedodger <sheila@moosedodger.com>
2016-06-29 14:45:23 -0600

    Pet Trip Ajax and functions form and more.

--- components/petTrip/petTrip.js
+++ components/petTrip/petTrip.js
@@ -1,34 +1,92 @@
 import React from 'react';
 
+
+
 var PetTrip = React.createClass({
+
+  getInitialState(){
+    return{
+      startDate: null,
+      endDate: null,
+      startPoint: null,
+      endPoint: null,
+      comments: null
+    }
+  },
+
+
+handleStartDateChange: function (e){
+  this.setState({startDate: e.target.value})
+},
+handleEndDateChange: function (e){
+  this.setState({endDate: e.target.value})
+},
+handleStartPointChange: function (e){
+  this.setState({startPoint: e.target.value})
+},
+handleEndPointChange: function (e){
+  this.setState({endPoint: e.target.value})
+},
+handleCommentsChange: function (e){
+  this.setState({comments: e.target.value})
+},
+
+handleFormSubmit: function(e){
+  e.preventDefault(); //prevents page refresh
+  var trip = {};
+  trip.startDate = this.state.startDate;
+  trip.endDate = this.state.endDate;
+  trip.startPoint = this.state.startPoint;
+  trip.endPoint = this.state.endPoint;
+  trip.comments = this.state.comments;
+
+  this.context.handleMongoId(trip, this.handlePetTripFormUpdate);
+  this.setState({ startDate: "", endDate: "", startPoint: "", endPoint: "", comments: ""});
+},
+
+handlePetTripFormUpdate: function (trip, mongoId){
+  $.ajax({
+    url: '/travel/' + mongoId,
+    method: 'PUT',
+    dataType: 'json',
+    data: trip,
+    success: funtion(data){
+    }.bind(this),
+      error: function(xhr, status, err){
+        console.error('/users/' + mongId, status, err.toString());
+      }.bind(this)
+  });
+},
+
+
  render: function(){
    return (
      <div>
        <div className="container">
-       <form className="form-inline" onSubmit={this.props.xxxx}>
+       <form className="form-inline" onSubmit={this.handleFormSubmit}>
            <div className="form-group">
              <label>Where are leaving from?</label>
              <input type="text" className="form-control" placeholder="Starting location"
-             onChange={this.props.handleUrlChange} value={this.props.url}/>
+             onChange={this.handleStartPointChange} value={this.state.startPoint}/>
            </div>
            <div className="form-group">
              <label>When are you leaving?</label>
              <input type="datetime-local" className="form-control"
-             onChange={this.props.handleUrlChange} value={this.props.url}/>
+             onChange={this.handleStartDateChange} value={this.startDate}/>
            </div>
            <div className="form-group">
              <label>Where are you going?</label>
-             <input type="text" className="form-control" placeholder="Destination" onChange={this.props.handleMiscChange}   value={this.props.misc}/>
+             <input type="text" className="form-control" placeholder="Destination" onChange={this.handleEndPointChange}   value={this.endPoint}/>
            </div>
            <div className="form-group">
              <label>When will you arrive?</label>
              <input type="datetime-local" className="form-control"
-             onChange={this.props.handleUrlChange} value={this.props.url}/>
+             onChange={this.handleEndDateChange} value={this.endDate}/>
            </div>
            <div className="form-group">
              <label>Is there any else you would like us to know?</label>
              <input type="textarea" className="form-control"
-             onChange={this.props.handleUrlChange} value={this.props.url}/>
+             onChange={this.handleCommentsChange} value={this.comments}/>
           </div>
              <button type="submit" className="btn btn-primary">Register</button>
          </form>
