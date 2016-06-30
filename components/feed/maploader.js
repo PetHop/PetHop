10df328364fb5acf9c3d10b14f277277f0e0d66f
Milton Menasco <milton@miltonmenasco.com>
2016-06-30 16:52:19 -0600

    Getting data from server to listings page

--- components/feed/maploader.js
+++ components/feed/maploader.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import ScriptjsLoader from 'react-google-maps/lib/async/ScriptjsLoader'
-
-function MapLoader (props){
-    return (
-        <ScriptjsLoader
-      hostname={"maps.googleapis.com"}
-      pathname={"/maps/api/js"}
-      query={ {
-        v: 3.24,
-        key: 'AIzaSyBJj27CHuv0PDk7y1qgsUodrWM3HCJyv4A',
-        libraries: "geometry,drawing,places"
-      } }
-      loadingElement={
-        <div>
-          <i className="fa fa-spinner" aria-hidden="true"></i>
-        </div>
-      }
-
-      containerElement={
-        <div id="google-map-container" />
-      }
-      googleMapElement={ props.children }
-    />
-        )
-};
-
-module.exports = MapLoader;
+// import React from 'react'
+// import ScriptjsLoader from 'react-google-maps/lib/async/ScriptjsLoader'
+//
+// function MapLoader (props){
+//     return (
+//         <ScriptjsLoader
+//       hostname={"maps.googleapis.com"}
+//       pathname={"/maps/api/js"}
+//       query={ {
+//         v: 3.24,
+//         key: 'AIzaSyBJj27CHuv0PDk7y1qgsUodrWM3HCJyv4A',
+//         libraries: "geometry,drawing,places"
+//       } }
+//       loadingElement={
+//         <div>
+//           <i className="fa fa-spinner" aria-hidden="true"></i>
+//         </div>
+//       }
+//
+//       containerElement={
+//         <div id="google-map-container" />
+//       }
+//       googleMapElement={ props.children }
+//     />
+//         )
+// };
+//
+// module.exports = MapLoader;
