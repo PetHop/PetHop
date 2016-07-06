cc5bf7a3189ec0db8ae305564e8e8f9262e18d8f
bbauer1702 <brian.bauer@inventati.org>
2016-07-06 11:15:08 -0600

    reverted from materialize inputs which did not register data and caused all posts to fail

--- components/petTrip/petOptionCard.js
+++ components/petTrip/petOptionCard.js
@@ -6,7 +6,7 @@ var PetOptionCard = React.createClass({
     return (
       <div>
         <p>PHOTO PLACEHOLDER</p>
-        <input type="checkbox" className="filled-in" id={ this.props.id } data-id={ this.props.id } value={ this.props.id } onChange={ this.props.handleTravelerCheckbox }/>
+        <input type="checkbox" className="filled-in" id={ this.props.id } value={ this.props.id } onChange={ this.props.handleTravelerCheckbox }/>
         <label htmlFor={ this.props.id }>{ this.props.name } the { this.props.breed } { this.props.type }</label>
       </div>
     );
