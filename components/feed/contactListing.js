8d2a9b91981cded8dacd075aa19f6666210e321d
bbauer1702 <brian.bauer@inventati.org>
2016-07-11 13:19:09 -0600

    contact card is setup

--- components/feed/contactListing.js
+++ components/feed/contactListing.js
@@ -28,9 +28,15 @@ var contactListing = React.createClass({
   },
 
   render: function(){
+    var mailto = 'mailto' + this.state.activeTravelData.tripPostedBy.email;
     return(
       <div>
-        <p> Future location of the contact (info) form! </p>
+        <h4>Great!</h4>
+        <p>We're glad you want to help!</p>
+        <p>Please contact { this.state.activeTravelData.tripPostedBy.firstName } { this.state.activeTravelData.tripPostedBy.lastName } by <a href={ mailto }>email</a> or telephone: { this.state.activeTravelData.tripPostedBy.phone } to make further arrangments.</p>
+        <img src={ this.state.activeTravelData.tripPostedBy.userImg } height="200" width="200" className="circle" />
+        <img src={ this.state.activeTravelData.animalTraveler.petProfileImage } height="200" width="200" className="circle" />
+        <h5>Thanks for using PetHop and enjoy your trip!</h5>
       </div>
     )
   }
