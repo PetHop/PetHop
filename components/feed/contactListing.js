8c6ebb8671a660a34ebe458e0261269668e78de3
Shawn Farnum <shawnmfarnum@gmail.com>
2016-07-15 22:47:50 -0600

    Merging Milton's map changes

--- components/feed/contactListing.js
+++ components/feed/contactListing.js
@@ -35,7 +35,6 @@ var contactListing = React.createClass({
         <p>We're glad you want to help!</p>
         <p>Please contact { this.state.activeTravelData.tripPostedBy.firstName } { this.state.activeTravelData.tripPostedBy.lastName } by <a href={ mailto }>email</a> or telephone: { this.state.activeTravelData.tripPostedBy.phone } to make further arrangments.</p>
         <img src={ this.state.activeTravelData.tripPostedBy.userImg } height="200" width="200" className="circle" />
-        <img src={ this.state.activeTravelData.animalTraveler.petProfileImage } height="200" width="200" className="circle" />
         <h5>Thanks for using PetHop and enjoy your trip!</h5>
       </div>
     ) : null;
--- components/feed/tripDetails.js
+++ components/feed/tripDetails.js
@@ -36,9 +36,31 @@ var Details = React.createClass({
     console.log(this.state.activeTravelData);
     var tripDetails = (this.state.activeTravelData) ? (
       <div className="valign-wrapper">
-        <div className="details col 6">
+        <div className="details col 12">
             <div className="col s12 m12 l6 container left">
-              <div className="card z-depth-2">
+              <div className="card">
+              <p className="flow-text" id="contact">
+                <img src={ this.state.activeTravelData.animalTraveler[0].petProfileImage } className="responsive-img circle center" id="detail-user" />
+                <h6> Can you help </h6><h5>{ this.state.activeTravelData.animalTraveler[0].petName }</h5><h6> Get from: { this.state.activeTravelData.startPoint } to { this.state.activeTravelData.endPoint }? </h6>
+                <h5> Additional Details: </h5>
+                <p> { this.state.activeTravelData.comments } </p>
+                <p> Request posted by:  { this.state.activeTravelData.tripPostedBy.firstName } { this.state.activeTravelData.tripPostedBy.lastName }</p>
+              </p>
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4"> { this.state.activeTravelData.animalTraveler[0].petName } <i className="material-icons right">more_vert</i></span>
+                </div>
+                 <div className="card-reveal">
+                  <span className="card-title grey-text text-darken-4">{ this.state.activeTravelData.animalTraveler[0].petName }<i className="material-icons right">close</i></span>
+                  <p className="flow-text" id="contact">
+                    <img src={ this.state.activeTravelData.animalTraveler[0].petProfileImage } className="responsive-img circle" id="detail-user" />
+                    <h6> Can you help </h6><h5>{ this.state.activeTravelData.animalTraveler[0].petName }</h5><h6> Get from: { this.state.activeTravelData.startPoint } to { this.state.activeTravelData.endPoint }? </h6>
+                    <h5> Additional Details: </h5>
+                    <p> { this.state.activeTravelData.comments } </p>
+                    <p> Request posted by:  { this.state.activeTravelData.tripPostedBy.firstName } { this.state.activeTravelData.tripPostedBy.lastName }</p>
+                  </p>
+                </div>
+              </div>
+              {/*<div className="card z-depth-2">
                 <div className="card-image waves-effect waves-block waves-light">
                 </div>
                   <div className="card-content center">
@@ -50,7 +72,11 @@ var Details = React.createClass({
                       <p> Request posted by:  { this.state.activeTravelData.tripPostedBy.firstName } { this.state.activeTravelData.tripPostedBy.lastName }</p>
                     </p>
                   </div>
-              </div>
+                  <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                  </div>
+              </div>*/}
           </div>
         </div>
       </div>
