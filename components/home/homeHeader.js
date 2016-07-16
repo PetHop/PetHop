4f3a3633410a906bba036358e37b62e134730b58
Milton Menasco <milton@miltonmenasco.com>
2016-07-15 22:47:11 -0600

    pushing homeheader conflict fixes

--- components/home/homeHeader.js
+++ components/home/homeHeader.js
@@ -22,16 +22,13 @@ var HomeHeader = React.createClass({
             <li>
               <img src='http://pethop.co/views/img/96428.png'/>
               <div className="caption center-align">
-              <img src={ slider2 }/>
-              <div className="http//pethop.co/views/img/96428.png">
-
                 <h3>Select a Trip Today!</h3>
                 <h5 className="light grey-text text-lighten-3">Our drivers are pet owners like you, they know how they would want their own pets to be treated.</h5>
                 <a href="#register" className="btn waves-effect waves-light col s12 m12 l6 blue">Sign Up Now</a>
               </div>
             </li>
-            <li>
 
+            <li>
               <img src='http://pethop.co/views/img/55609.png'/>
               <div className="caption center-align top">
                 <h3>Tagline 3!</h3>
